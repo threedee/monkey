5ef47ace0ea2303afb0708042fb2bac8a8f9e1b1
Dennis Engelbrecht <dennis@engelbrecht.org>
2021-03-08 10:42:51 +0100

    Introduced parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/threedee/monkey/ast"
+	"github.com/threedee/monkey/lexer"
+	"github.com/threedee/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read tow tokens, so curToken and peekToken are both set
+	p.NextToken()
+	p.NextToken()
+
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
