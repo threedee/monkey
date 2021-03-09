cc1849c676af2139ad383e40c31e3923a69a79cd
Dennis Engelbrecht <dennis@engelbrecht.org>
2021-03-09 10:26:05 +0100

    Errorhanling for parsing

--- parser/parser.go
+++ parser/parser.go
@@ -1,26 +1,36 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/threedee/monkey/ast"
 	"github.com/threedee/monkey/lexer"
 	"github.com/threedee/monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read tow tokens, so curToken and peekToken are both set
 	p.NextToken()
 	p.NextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) NextToken() {
@@ -51,7 +61,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
-func (p *Parser) parseStatement() *ast.LetStatement {
+func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
 	if !p.expectPeek(token.IDENT) {
@@ -86,9 +96,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.NextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 
 	//Output:
 
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
